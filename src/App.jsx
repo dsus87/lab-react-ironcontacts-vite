bffbc12d58f9242e14aac9168235c3890f6077f6
Daniel Susetio <dsusetio87@gmail.com>
2024-02-04 11:22:27 +0100

    iterations 1-4

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,61 @@ function App() {
 
  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
 
+ // Add Random Contact function
+
+ const addRandomContact = () => {
+
+// Filter out contacts that are already being displayed
+
+  const remainingContacts = contactsData.filter(contact => 
+    !contacts.find(c => c.id === contact.id));
+    if (remainingContacts.length === 0) {
+      alert("No more contacts to add.");
+      return;
+    }
+
+// Select a random contact from the remaining contacts
+
+ const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+
+
+// Update the contacts state by adding the new random contact
+// Use the spread operator to include the current contacts and add the new one
+    setContacts([...contacts, randomContact]);
+  };
+
+
+// Sort by Name function
+
+
+
+
+const sortByName = () => {
+  const sortedContacts = [...contacts].sort((a, b) =>  // use spread operator to create copy prevents mutalating original state
+  a.name.localeCompare(b.name));
+  setContacts(sortedContacts);  // setContacts function is used to update the state with the new sorted array. This triggers a re-render of the component.
+};
+
+// Sort by Popularity function
+const sortByPopularity = () => { 
+  const sortedContacts = [...contacts].sort((a, b) => 
+  b.popularity - a.popularity);  // sorts the contacts by popularity in descending order (highest first) by subtracting
+  setContacts(sortedContacts);
+};
+
+
+
+
  return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+
+
+
       <table>
         <thead>
           <tr>
